d1d949f2d986383106e5976eecba0acca426f294
Martin Hoffmann <hn@nvnc.de>
2016-12-14 14:38:17 +0100

    Rearrange flags.

--- pkcs11/src/cryptoki/flags.rs
+++ pkcs11/src/cryptoki/flags.rs
@@ -3,303 +3,318 @@ use std::ops;
 use pkcs11_sys as sys;
 
 
-//------------ MechanismInfoFlags --------------------------------------------
+//------------ The Macro for Making the Structs ------------------------------
 
-/// Flags specifying mechanism capabilities.
-#[derive(Clone, Copy, Debug, Default)]
-pub struct MechanismInfoFlags(sys::CK_FLAGS);
+macro_rules! ck_flags {
+    ( $(#[$attr:meta])*
+      pub struct $typename:ident {
+         $(
+             $(#[$item_attr:meta])* flag $item:ident: $value:expr 
+         ),*
+      }
+    ) => {
+        $(#[$attr])*
+        #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord,
+                 PartialEq, PartialOrd)]
+        pub struct $typename(sys::CK_FLAGS);
 
-impl MechanismInfoFlags {
-    /// Returns whether the mechanism is performed by the device.
-    pub fn hw(&self) -> bool {
-        self.0 & sys::CKF_HW != 0
-    }
+        /// # Constructors for Variants
+        ///
+        impl $typename {
+            $(
+                $(#[$item_attr])*
+                pub fn $item() -> Self { $typename($value) }
+            )*
+        }
 
-    /// Returns whether the mechanism can be used for encryption.
-    pub fn encrypt(&self) -> bool {
-        self.0 & sys::CKF_ENCRYPT != 0
-    }
+        /// # Other Methods
+        impl $typename {
+            pub fn empty() -> Self { $typename(0) }
 
-    /// Returns whether the mechanism can be used for decryption.
-    pub fn decrypt(&self) -> bool {
-        self.0 & sys::CKF_DECRYPT != 0
-    }
+            pub fn all() -> Self {
+                $typename($( $value )|*)
+            }
 
-    /// Returns whether the mechanism can be used for digests.
-    pub fn digest(&self) -> bool {
-        self.0 & sys::CKF_DIGEST != 0
-    }
+            pub fn is_empty(self) -> bool {
+                self.0 == 0
+            }
 
-    /// Returns whether the mechanism can be used for signing.
-    pub fn sign(&self) -> bool {
-        self.0 & sys::CKF_SIGN != 0
-    }
+            pub fn is_all(self) -> bool {
+                self == Self::all()
+            }
 
-    /// Returns whether the mechanism can be used for recovering signing.
-    pub fn sign_recover(&self) -> bool {
-        self.0 & sys::CKF_SIGN_RECOVER != 0
-    }
+            pub fn intersects(self, other: Self) -> bool {
+                !(self & other).is_empty()
+            }
 
-    /// Returns whether the mechanism can be used for verification.
-    pub fn verify(&self) -> bool {
-        self.0 & sys::CKF_VERIFY != 0
-    }
+            pub fn contains(self, other: Self) -> bool {
+                (self & other) == other
+            }
+        }
 
-    /// Returns whether the mechanism can be used for recovering verification.
-    pub fn verify_recover(&self) -> bool {
-        self.0 & sys::CKF_VERIFY_RECOVER != 0
-    }
+        impl From<sys::CK_FLAGS> for $typename {
+            fn from(value: sys::CK_FLAGS) -> Self {
+                $typename(value) & $typename::all()
+            }
+        }
 
-    /// Returns whether the mechanism can be used to generate a secret key.
-    pub fn generate(&self) -> bool {
-        self.0 & sys::CKF_GENERATE != 0
-    }
+        impl From<$typename> for sys::CK_FLAGS {
+            fn from(value: $typename) -> Self {
+                value.0
+            }
+        }
 
-    /// Returns whether the mechanism can be used to generate a key pair.
-    pub fn generate_key_pair(&self) -> bool {
-        self.0 & sys::CKF_GENERATE_KEY_PAIR != 0
-    }
+        impl ops::BitOr for $typename {
+            type Output = Self;
 
-    /// Returns whether the mechanism can be used to wrap a key.
-    pub fn wrap(&self) -> bool {
-        self.0 & sys::CKF_WRAP != 0
-    }
+            fn bitor(self, other: Self) -> Self {
+                $typename(self.0 | other.0)
+            }
+        }
 
-    /// Returns whether the mechanism can be used to unwrap a key.
-    pub fn unwrap(&self) -> bool {
-        self.0 & sys::CKF_UNWRAP != 0
-    }
+        impl ops::BitOrAssign for $typename {
+            fn bitor_assign(&mut self, other: Self) {
+                self.0 |= other.0
+            }
+        }
 
-    /// Returns whether the mechanism can be used to derive a key.
-    pub fn derive(&self) -> bool {
-        self.0 & sys::CKF_DERIVE != 0
+        impl ops::BitAnd for $typename {
+            type Output = Self;
+
+            fn bitand(self, other: Self) -> Self {
+                $typename(self.0 & other.0)
+            }
+        }
+
+        impl ops::BitAndAssign for $typename {
+            fn bitand_assign(&mut self, other: Self) {
+                self.0 &= other.0
+            }
+        }
+
+        impl ops::BitXor for $typename {
+            type Output = Self;
+
+            fn bitxor(self, other: Self) -> Self {
+                $typename(self.0 ^ other.0)
+            }
+        }
+
+        impl ops::BitXorAssign for $typename {
+            fn bitxor_assign(&mut self, other: Self) {
+                self.0 ^= other.0
+            }
+        }
+
+        impl ops::Sub for $typename {
+            type Output = Self;
+
+            fn sub(self, other: Self) -> Self {
+                $typename(self.0 & ! other.0)
+            }
+        }
+
+        impl ops::SubAssign for $typename {
+            fn sub_assign(&mut self, other: Self) {
+                self.0 &= !other.0
+            }
+        }
+
+        impl ops::Not for $typename {
+            type Output = Self;
+
+            fn not(self) -> Self {
+                $typename(!self.0) & $typename::all()
+            }
+        }
     }
 }
 
-impl From<sys::CK_FLAGS> for MechanismInfoFlags {
-    fn from(value: sys::CK_FLAGS) -> Self {
-        MechanismInfoFlags(value)
+
+//------------ InitializeFlags -----------------------------------------------
+
+ck_flags!{
+    /// Flags specifying options when intializing the library.
+    pub struct InitializeFlags {
+        /// Set if the library is not allowed to spawn OS threads.
+        flag library_cant_create_os_threads:
+                                     sys::CKF_LIBRARY_CANT_CREATE_OS_THREADS,
+        /// Set if the library should use locking provided by the OS.
+        flag os_locking_ok: sys::CKF_OS_LOCKING_OK
+    }
+}
+
+
+//------------ MechanismFlags --------------------------------------------
+
+ck_flags!{
+    /// Flags specifying mechanism capabilities.
+    pub struct MechanismFlags {
+        /// Set if the mechanism is performed by the device.
+        flag hw: sys::CKF_HW,
+
+        /// Set if the mechanism can be used for encryption.
+        flag encrypt: sys::CKF_ENCRYPT,
+
+        /// Set if the mechanism can be used for decryption.
+        flag decrypt: sys::CKF_DECRYPT,
+
+        /// Set if the mechanism can be used for digests.
+        flag digest: sys::CKF_DIGEST,
+
+        /// Set if the mechanism can be used for signing.
+        flag sign: sys::CKF_SIGN,
+
+        /// Set if the mechanism can be used for recovering signing.
+        flag sign_recover: sys::CKF_SIGN_RECOVER,
+
+        /// Set if the mechanism can be used for verification.
+        flag verify: sys::CKF_VERIFY,
+
+        /// Set if the mechanism can be used for recovering verification.
+        flag verify_recover: sys::CKF_VERIFY_RECOVER,
+
+        /// Set if the mechanism can be used to generate a secret key.
+        flag generate: sys::CKF_GENERATE,
+
+        /// Set if the mechanism can be used to generate a key pair.
+        flag generate_key_pair: sys::CKF_GENERATE_KEY_PAIR,
+
+        /// Set if the mechanism can be used to wrap a key.
+        flag wrap: sys::CKF_WRAP,
+
+        /// Set if the mechanism can be used to unwrap a key.
+        flag unwrap: sys::CKF_UNWRAP,
+
+        /// Set if the mechanism can be used to derive a key.
+        flag derive: sys::CKF_DERIVE
     }
 }
 
 
 //------------ SessionFlags --------------------------------------------------
 
-/// The flags defining the type of a session.
-#[derive(Clone, Copy, Debug, Default)]
-pub struct SessionFlags(sys::CK_FLAGS);
+ck_flags! {
+    /// The flags defining the type of a session.
+    pub struct SessionFlags {
+        /// Set if the session is read/write, unset if it is read-only.
+        flag rw_session: sys::CKF_RW_SESSION,
 
-impl SessionFlags {
-    pub fn rw_session() -> Self {
-        SessionFlags(sys::CKF_RW_SESSION)
-    }
-
-    pub fn serial_session() -> Self {
-        SessionFlags(sys::CKF_SERIAL_SESSION)
-    }
-
-    pub fn is_rw_session(self) -> bool {
-        self.0 & sys::CKF_RW_SESSION != 0
-    }
-
-    pub fn is_serial_session(self) -> bool {
-        self.0 & sys::CKF_SERIAL_SESSION != 0
-    }
-}
-
-impl From<sys::CK_FLAGS> for SessionFlags {
-    fn from(flags: sys::CK_FLAGS) -> Self {
-        SessionFlags(flags)
-    }
-}
-
-impl From<SessionFlags> for sys::CK_FLAGS {
-    fn from(flags: SessionFlags) -> Self {
-        flags.0
-    }
-}
-
-impl ops::BitOr for SessionFlags {
-    type Output = Self;
-
-    fn bitor(self, other: Self) -> Self {
-        SessionFlags(self.0 | other.0)
+        /// Set if the session is to be used by one thread only.
+        ///
+        /// This flag must always be set in this version of Cryptoki. It is
+        /// provided for backwards compatibility only.
+        flag serial_session: sys::CKF_SERIAL_SESSION
     }
 }
 
 
-//------------ SlotInfoFlags -------------------------------------------------
+//------------ SlotFlags -------------------------------------------------
 
-/// The flags of a [SlotInfo] struct.
-///
-/// [SlotInfo]: struct.SlotInfo.html
-#[derive(Clone, Copy, Debug, Default)]
-pub struct SlotInfoFlags(sys::CK_FLAGS);
-
-impl SlotInfoFlags {
-    /// Returns whether a token is present in the slot.
-    pub fn token_present(&self) -> bool {
-        self.0 & sys::CKF_TOKEN_PRESENT != 0
-    }
-
-    /// Returns whether the reader supports removable devices.
-    pub fn removable_device(&self) -> bool {
-        self.0 & sys::CKF_REMOVABLE_DEVICE != 0
-    }
-
-    /// Returns whether the slot is a hardware slot.
+ck_flags! {
+    /// The flags of a [SlotInfo] struct.
     ///
-    /// Otherwise, it is a software slot implementing a ”soft token.”
-    pub fn hw_slot(&self) -> bool {
-        self.0 & sys::CKF_HW_SLOW != 0
-    }
-}
+    /// [SlotInfo]: struct.SlotInfo.html
+    pub struct SlotFlags {
+        /// Set if a token is present in the slot.
+        flag token_present: sys::CKF_TOKEN_PRESENT,
 
-impl From<sys::CK_FLAGS> for SlotInfoFlags {
-    fn from(flags: sys::CK_FLAGS) -> Self {
-        SlotInfoFlags(flags)
+        /// Set if the reader supports removable devices.
+        flag removable_device: sys::CKF_REMOVABLE_DEVICE,
+
+        /// Set if the slot is a hardware slot.
+        ///
+        /// Otherwise, it is a software slot implementing a ”soft token.”
+        flag hw_slot: sys::CKF_HW_SLOW
     }
 }
 
 
-//------------ TokenInfoFlags ------------------------------------------------
+//------------ TokenFlags ------------------------------------------------
 
-/// The flags indicating capabilities and status of a token.
-#[derive(Clone, Copy, Debug, Default)]
-pub struct TokenInfoFlags(sys::CK_FLAGS);
+ck_flags! {
+    /// The flags indicating capabilities and status of a token.
+    pub struct TokenFlags {
+        /// Set if the token has a random number generator.
+        flag rng: sys::CKF_RNG,
 
-impl TokenInfoFlags {
-    /// Returns whether the token has a random number generator.
-    pub fn rng(&self) -> bool {
-        self.0 & sys::CKF_RNG != 0
-    }
+        /// Set if the token is write-protected.
+        flag write_protected: sys::CKF_WRITE_PROTECTED,
 
-    /// Returns whether the token is write-protected.
-    ///
-    /// Exactly what ’write-protected’ means is not specified in Cryptoki.
-    /// An application may be unable to perform certain actions on a
-    /// write-protected token. A token may change whether it is
-    /// write-protected depending on the session state. For instance, it
-    /// may remain write-protected until after a successful login.
-    pub fn write_protected(&self) -> bool {
-        self.0 & sys::CKF_WRITE_PROTECTED != 0
-    }
+        /// Set if there are functions for which a user must be logged in.
+        flag login_required: sys::CKF_LOGIN_REQUIRED,
 
-    /// Returns whether there are functions for which a user must be logged in.
-    pub fn login_required(&self) -> bool {
-        self.0 & sys::CKF_LOGIN_REQUIRED != 0
-    }
+        /// Set if the normal user’s PIN has been initialized.
+        flag user_pin_initialized: sys::CKF_USER_PIN_INITIALIZED,
 
-    /// Returns whether the normal user’s PIN has been initialized.
-    pub fn user_pin_initialized(&self) -> bool {
-        self.0 & sys::CKF_USER_PIN_INITIALIZED != 0
-    }
+        /// Set if saved session state contains all keys for restore.
+        flag restore_key_not_needed: sys::CKF_RESTORE_KEY_NOT_NEEDED,
 
-    /// Returns whether saved session state contains all keys for restore.
-    pub fn restore_key_not_needed(&self) -> bool {
-        self.0 & sys::CKF_RESTORE_KEY_NOT_NEEDED != 0
-    }
+        /// Set if the token has its own hardware clock.
+        flag clock_on_token: sys::CKF_CLOCK_ON_TOKEN,
 
-    /// Returns whether the token has its own hardware clock.
-    pub fn clock_on_token(&self) -> bool {
-        self.0 & sys::CKF_CLOCK_ON_TOKEN != 0
-    }
+        /// Set if the token has a “protected authentication path.”
+        ///
+        /// If it does, a user can log into a token without passing a PIN
+        /// through the Cryptoki library.
+        flag protected_authentication_path:
+            sys::CKF_PROTECTED_AUTHENTICATION_PATH,
 
-    /// Returns whether the token has a “protected authentication path.”
-    ///
-    /// If it does, a user can log into a token without passing a PIN
-    /// through the Cryptoki library.
-    pub fn protected_authentication_path(&self) -> bool {
-        self.0 & sys::CKF_PROTECTED_AUTHENTICATION_PATH != 0
-    }
+        /// Set if the token supports dual crypto operations.
+        flag dual_crypto_operations: sys::CKF_DUAL_CRYPTO_OPERATIONS,
 
-    /// Returns whether the token supports dual crypto operations.
-    pub fn dual_crypto_operations(&self) -> bool {
-        self.0 & sys::CKF_DUAL_CRYPTO_OPERATIONS != 0
-    }
+        /// Set if the token has been intialized.
+        flag token_initialized: sys::CKF_TOKEN_INITIALIZED,
 
-    /// Returns whether the token has been intialized.
-    ///
-    /// If this returns `true` and `Cryptoki::init_token()` is called, the
-    /// token will be re-initialized.
-    pub fn token_initialized(&self) -> bool {
-        self.0 & sys::CKF_TOKEN_INITIALIZED != 0
-    }
+        /// Set if the token supports secondary authentication for
+        /// private key objects.
+        ///
+        /// This flag is deprecated and should not be set.
+        flag secondary_authentication: sys::CKF_SECONDARY_AUTHENTICATION,
 
-    /// Returns whether the token supports secondary authentication for
-    /// private key objects.
-    ///
-    /// This flag is deprecated and should not be set.
-    pub fn secondary_authentication(&self) -> bool {
-        self.0 & sys::CKF_SECONDARY_AUTHENTICATION != 0
-    }
+        /// Set if an incorrect user PIN has been entered at least once
+        /// since last successful authentication.
+        flag user_pin_count_low: sys::CKF_USER_PIN_COUNT_LOW,
 
-    /// Returns whether an incorrect user PIN has been entered at least once
-    /// since last successful authentication.
-    pub fn user_pin_count_low(&self) -> bool {
-        self.0 & sys::CKF_USER_PIN_COUNT_LOW != 0
-    }
+        /// Set if supplying an incorrect user PIN will cause it to
+        /// become locked.
+        flag user_pin_final_try: sys::CKF_USER_PIN_FINAL_TRY,
 
-    /// Returns whether supplying an incorrect user PIN will cause it to
-    /// become locked.
-    pub fn user_pin_final_try(&self) -> bool {
-        self.0 & sys::CKF_USER_PIN_FINAL_TRY != 0
-    }
+        /// Set if the user PIN has been locked.
+        ///
+        /// User login is not possible into a token with a locked user PIN.
+        flag user_pin_locked: sys::CKF_USER_PIN_LOCKED,
 
-    /// Returns whether the user PIN has been locked.
-    ///
-    /// User login is not possible into a token with a locked user PIN.
-    pub fn user_pin_locked(&self) -> bool {
-        self.0 & sys::CKF_USER_PIN_LOCKED != 0
-    }
+        /// Set if the user PIN needs to be changed.
+        ///
+        /// This happens if the PIN value is the default value set by token
+        /// initialization or manufacturing, or the PIN has been expired by the
+        /// card.
+        flag user_pin_to_be_changed: sys::CKF_USER_PIN_TO_BE_CHANGED,
 
-    /// Returns whether the user PIN needs to be changed.
-    ///
-    /// This happens if the PIN value is the default value set by token
-    /// initialization or manufacturing, or the PIN has been expired by the
-    /// card.
-    pub fn user_pin_to_be_changed(&self) -> bool {
-        self.0 & sys::CKF_USER_PIN_TO_BE_CHANGED != 0
-    }
+        /// Set if an incorrect SO PIN has been entered at least once
+        /// since last successful authentication.
+        flag so_pin_count_low: sys::CKF_SO_PIN_COUNT_LOW,
 
-    /// Returns whether an incorrect SO PIN has been entered at least once
-    /// since last successful authentication.
-    pub fn so_pin_count_low(&self) -> bool {
-        self.0 & sys::CKF_SO_PIN_COUNT_LOW != 0
-    }
+        /// Set if supplying an incorrect SO PIN will cause it to
+        /// become locked.
+        flag so_pin_final_try: sys::CKF_SO_PIN_FINAL_TRY,
 
-    /// Returns whether supplying an incorrect SO PIN will cause it to
-    /// become locked.
-    pub fn so_pin_final_try(&self) -> bool {
-        self.0 & sys::CKF_SO_PIN_FINAL_TRY != 0
-    }
+        /// Set if the SO PIN has been locked.
+        ///
+        /// SO login is not possible into a token with a locked SO PIN.
+        flag so_pin_locked: sys::CKF_SO_PIN_LOCKED,
 
-    /// Returns whether the SO PIN has been locked.
-    ///
-    /// SO login is not possible into a token with a locked SO PIN.
-    pub fn so_pin_locked(&self) -> bool {
-        self.0 & sys::CKF_SO_PIN_LOCKED != 0
-    }
+        /// Set if the SO PIN needs to be changed.
+        ///
+        /// This happens if the PIN value is the default value set by token
+        /// initialization or manufacturing, or the PIN has been expired by the
+        /// card.
+        flag so_pin_to_be_changed: sys::CKF_SO_PIN_TO_BE_CHANGED,
 
-    /// Returns whether the SO PIN needs to be changed.
-    ///
-    /// This happens if the PIN value is the default value set by token
-    /// initialization or manufacturing, or the PIN has been expired by the
-    /// card.
-    pub fn so_pin_to_be_changed(&self) -> bool {
-        self.0 & sys::CKF_SO_PIN_TO_BE_CHANGED != 0
-    }
-
-    /// Returns whether the token failed a FIPS 140-2 self-test and entered
-    /// an error state.
-    pub fn error_state(&self) -> bool {
-        self.0 & sys::CKF_ERROR_STATE != 0
-    }
-}
-
-impl From<sys::CK_FLAGS> for TokenInfoFlags {
-    fn from(flags: sys::CK_FLAGS) -> Self {
-        TokenInfoFlags(flags)
+        /// Set if the token failed a FIPS 140-2 self-test and entered
+        /// an error state.
+        flag error_state: sys::CKF_ERROR_STATE
     }
 }
 
